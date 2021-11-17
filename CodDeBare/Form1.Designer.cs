199419b6e23b27d9cb7fc30d206bfb5b37dc2d14
Vasile Balaceanu <vasile.balaceanu98@gmail.com>
2021-11-17 15:35:24 +0200

    Barcode Generator

---
+++ CodDeBare/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace CodDeBare
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ButonGenereaza = new System.Windows.Forms.Button();
+            this.ButonSalveaza = new System.Windows.Forms.Button();
+            this.valintrodusa = new System.Windows.Forms.TextBox();
+            this.valoare = new System.Windows.Forms.Label();
+            this.pb = new System.Windows.Forms.PictureBox();
+            this.coddeafisat = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.aparechenar = new System.Windows.Forms.CheckBox();
+            this.butonopenfile = new System.Windows.Forms.Button();
+            this.txtBarcode = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pb)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ButonGenereaza
+            // 
+            this.ButonGenereaza.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ButonGenereaza.Location = new System.Drawing.Point(77, 284);
+            this.ButonGenereaza.Name = "ButonGenereaza";
+            this.ButonGenereaza.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.ButonGenereaza.Size = new System.Drawing.Size(120, 45);
+            this.ButonGenereaza.TabIndex = 2;
+            this.ButonGenereaza.Text = "Genereaza";
+            this.ButonGenereaza.UseVisualStyleBackColor = true;
+            this.ButonGenereaza.Click += new System.EventHandler(this.ButonGenereaza_Click);
+            // 
+            // ButonSalveaza
+            // 
+            this.ButonSalveaza.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ButonSalveaza.Location = new System.Drawing.Point(294, 284);
+            this.ButonSalveaza.Name = "ButonSalveaza";
+            this.ButonSalveaza.Size = new System.Drawing.Size(120, 45);
+            this.ButonSalveaza.TabIndex = 3;
+            this.ButonSalveaza.Text = "Salveaza";
+            this.ButonSalveaza.UseVisualStyleBackColor = true;
+            this.ButonSalveaza.Click += new System.EventHandler(this.ButonSalveaza_Click);
+            // 
+            // valintrodusa
+            // 
+            this.valintrodusa.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.valintrodusa.Location = new System.Drawing.Point(125, 217);
+            this.valintrodusa.MaxLength = 50;
+            this.valintrodusa.Multiline = true;
+            this.valintrodusa.Name = "valintrodusa";
+            this.valintrodusa.Size = new System.Drawing.Size(150, 22);
+            this.valintrodusa.TabIndex = 1;
+            this.valintrodusa.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.valintrodusa.TextChanged += new System.EventHandler(this.valintrodusa_TextChanged);
+            this.valintrodusa.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.valintrodusa_KeyPress);
+            // 
+            // valoare
+            // 
+            this.valoare.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.valoare.Location = new System.Drawing.Point(26, 209);
+            this.valoare.Name = "valoare";
+            this.valoare.Size = new System.Drawing.Size(99, 34);
+            this.valoare.TabIndex = 3;
+            this.valoare.Text = "Valoare";
+            this.valoare.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pb
+            // 
+            this.pb.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pb.Location = new System.Drawing.Point(41, 34);
+            this.pb.Name = "pb";
+            this.pb.Size = new System.Drawing.Size(400, 100);
+            this.pb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb.TabIndex = 4;
+            this.pb.TabStop = false;
+            this.pb.Paint += new System.Windows.Forms.PaintEventHandler(this.pb_Paint);
+            // 
+            // coddeafisat
+            // 
+            this.coddeafisat.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.coddeafisat.Location = new System.Drawing.Point(322, 218);
+            this.coddeafisat.Multiline = true;
+            this.coddeafisat.Name = "coddeafisat";
+            this.coddeafisat.Size = new System.Drawing.Size(150, 22);
+            this.coddeafisat.TabIndex = 5;
+            this.coddeafisat.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(281, 218);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 22);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "-->";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // aparechenar
+            // 
+            this.aparechenar.AutoSize = true;
+            this.aparechenar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.aparechenar.Location = new System.Drawing.Point(77, 153);
+            this.aparechenar.Name = "aparechenar";
+            this.aparechenar.Size = new System.Drawing.Size(92, 20);
+            this.aparechenar.TabIndex = 7;
+            this.aparechenar.Text = "Chenar alb";
+            this.aparechenar.UseVisualStyleBackColor = true;
+            this.aparechenar.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // butonopenfile
+            // 
+            this.butonopenfile.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.butonopenfile.Location = new System.Drawing.Point(77, 335);
+            this.butonopenfile.Name = "butonopenfile";
+            this.butonopenfile.Size = new System.Drawing.Size(120, 23);
+            this.butonopenfile.TabIndex = 8;
+            this.butonopenfile.Text = "OpenFile";
+            this.butonopenfile.UseVisualStyleBackColor = true;
+            this.butonopenfile.Click += new System.EventHandler(this.butonopenfile_Click);
+            // 
+            // txtBarcode
+            // 
+            this.txtBarcode.Location = new System.Drawing.Point(256, 335);
+            this.txtBarcode.Name = "txtBarcode";
+            this.txtBarcode.Size = new System.Drawing.Size(158, 20);
+            this.txtBarcode.TabIndex = 10;
+            this.txtBarcode.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 360);
+            this.Controls.Add(this.txtBarcode);
+            this.Controls.Add(this.butonopenfile);
+            this.Controls.Add(this.aparechenar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.coddeafisat);
+            this.Controls.Add(this.pb);
+            this.Controls.Add(this.valoare);
+            this.Controls.Add(this.valintrodusa);
+            this.Controls.Add(this.ButonSalveaza);
+            this.Controls.Add(this.ButonGenereaza);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Generator cod bare";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pb)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButonGenereaza;
+        private System.Windows.Forms.Button ButonSalveaza;
+        private System.Windows.Forms.TextBox valintrodusa;
+        private System.Windows.Forms.Label valoare;
+        private System.Windows.Forms.PictureBox pb;
+        private System.Windows.Forms.TextBox coddeafisat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox aparechenar;
+        private System.Windows.Forms.Button butonopenfile;
+        private System.Windows.Forms.TextBox txtBarcode;
+    }
+}
+
---
+++ CodDeBare/Form1.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using ZXing;
+
+namespace CodDeBare
+{
+    public partial class Form1 : Form
+    {
+        Image img;
+        Graphics g;
+        int sumapare = 0, sumaimpare = 0, i, j, lungimetextprimit, sumaparesiimpare,cheie;
+        long nrprimit;
+        int[,] cod;
+        int latimegros = 4, latimesubtire = 2, inaltime = 40, latimebare1cifra, latimebarecifre; //pentru desenarea barelor
+        int xstartbare, ystartbare, xtextdescris, ytextdescris,xbareretinut,ybareretinut,xfinalbare;
+
+       
+
+        bool periefolositaneagra = true, nrparcifreok=false;
+        SolidBrush perieneagra = new SolidBrush(Color.Black);
+        SolidBrush periealba = new SolidBrush(Color.White);
+        Font fontt = new Font("Times New Roman", 13);
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void valintrodusa_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(valintrodusa.Text))
+            {
+                ButonGenereaza.Enabled = false;
+                aparechenar.Enabled = false;
+            }
+            else
+            {
+                aparechenar.Enabled = true;
+                ButonGenereaza.Enabled = true;
+                Cursor.Current = Cursors.Default;
+            }
+        }
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            calculeaza();
+            imaginenoua();
+            genereaza(g);
+        }
+        private void valintrodusa_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {//testeaza daca tasta apasata este CTRL ori vreun caracter non-numeric
+                e.Handled = true;
+            }
+        }
+        private void ButonGenereaza_Click(object sender, EventArgs e)
+        {
+            calculeaza(); 
+            imaginenoua();
+            genereaza(g);
+        }
+        private void ButonSalveaza_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sf = new SaveFileDialog();
+            sf.Filter = "JPG(*.JPG) | *.jpg";
+
+
+            if (sf.ShowDialog() == DialogResult.OK)
+            {
+                img.Save(sf.FileName);
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ButonSalveaza.Enabled = false;
+            ButonGenereaza.Enabled = false;
+            aparechenar.Enabled = false;
+            img = new Bitmap(pb.Width, pb.Height);   
+            g = Graphics.FromImage(img);
+            //g.Clear(Color.LightBlue);
+        }
+        private void pb_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(img, 0, 0);
+        }
+        void calculeaza()
+        {
+            lungimetextprimit = valintrodusa.Text.Length;
+            if (lungimetextprimit % 2 == 0)
+            {
+                nrparcifreok = true;
+                coddeafisat.Clear();
+                ButonSalveaza.Enabled = true;
+                
+                latimebare1cifra = latimegros * 2 + latimesubtire * 3; // pentru o cifra
+                latimebarecifre = latimebare1cifra * (lungimetextprimit + 2); // pentru toate cifrele ce vor urma reprezentate
+
+                xstartbare = (pb.Width - latimebarecifre) / 2;
+                ystartbare = (pb.Height - inaltime) / 2;
+
+                xbareretinut = xstartbare;
+                ybareretinut = ystartbare;
+
+
+                nrprimit = Int64.Parse(valintrodusa.Text);
+                cod = new int[6, lungimetextprimit + 2]; // de ex lungime = 4 luam cod[6] adica pozitii de la 0 la 5. Pozitia 0 va fi cu 0, cod[6]=cheia, restul sunt nr primite
+                for (i = lungimetextprimit; i >= 0; i--)
+                {
+                    if (i != 0)
+                    {
+                        cod[0, i] = Convert.ToInt32(nrprimit % 10);
+                        nrprimit = nrprimit / 10;
+                    }
+                    else
+                        cod[0, i] = 0;//cod[0,0] ia valoarea 0;
+                }
+
+                for (i = 1; i <= lungimetextprimit; i = i + 2)
+                {
+                    sumaimpare = sumaimpare + cod[0, i];//suma cifrelor de pe pozitiile impare
+                }
+
+                for (i = 2; i <= lungimetextprimit; i = i + 2)
+                {
+                    sumapare = sumapare + cod[0, i];//suma cifrelor de pe pozitiile pare
+                }
+                sumaparesiimpare = sumaimpare + sumapare * 3; // suma impare + (pare*3)
+
+                for (i = 0; i <= 9; i++)
+                {
+                    if ((sumaparesiimpare + i) % 10 == 0)
+                    {
+                        cheie = i; // cautare cheie
+                        break;
+                    }
+                }
+
+                cod[0, lungimetextprimit + 1] = cheie;
+
+                for (i = 0; i < lungimetextprimit + 2; i++)
+                {
+                    if (cod[0, i] == 0)
+                    {
+                        cod[1, i] = 0; cod[2, i] = 0; cod[3, i] = 1; cod[4, i] = 1; cod[5, i] = 0;
+                    }
+                    if (cod[0, i] == 1)
+                    {
+                        cod[1, i] = 1; cod[2, i] = 0; cod[3, i] = 0; cod[4, i] = 0; cod[5, i] = 1;
+                    }
+                    if (cod[0, i] == 2)
+                    {
+                        cod[1, i] = 0; cod[2, i] = 1; cod[3, i] = 0; cod[4, i] = 0; cod[5, i] = 1;
+                    }
+                    if (cod[0, i] == 3)
+                    {
+                        cod[1, i] = 1; cod[2, i] = 1; cod[3, i] = 0; cod[4, i] = 0; cod[5, i] = 0;
+                    }
+                    if (cod[0, i] == 4)
+                    {
+                        cod[1, i] = 0; cod[2, i] = 0; cod[3, i] = 1; cod[4, i] = 0; cod[5, i] = 1;
+                    }
+                    if (cod[0, i] == 5)
+                    {
+                        cod[1, i] = 1; cod[2, i] = 0; cod[3, i] = 1; cod[4, i] = 0; cod[5, i] = 0;
+                    }
+                    if (cod[0, i] == 6)
+                    {
+                        cod[1, i] = 0; cod[2, i] = 1; cod[3, i] = 1; cod[4, i] = 0; cod[5, i] = 0;
+                    }
+                    if (cod[0, i] == 7)
+                    {
+                        cod[1, i] = 0; cod[2, i] = 0; cod[3, i] = 0; cod[4, i] = 1; cod[5, i] = 1;
+                    }
+                    if (cod[0, i] == 8)
+                    {
+                        cod[1, i] = 1; cod[2, i] = 0; cod[3, i] = 0; cod[4, i] = 1; cod[5, i] = 0;
+                    }
+                    if (cod[0, i] == 9)
+                    {
+                        cod[1, i] = 0; cod[2, i] = 1; cod[3, i] = 0; cod[4, i] = 1; cod[5, i] = 0;
+                    }
+                } //fiecarei cifre primite i am asociat codul binar pt bare   
+
+                for (i = 0; i < lungimetextprimit + 2; i++)
+                {
+                    coddeafisat.Text = coddeafisat.Text + cod[0, i].ToString();
+                } // scriere in coddeafisat din dreapta 
+            }
+            else
+            {
+                MessageBox.Show("Te rugam sa introduci un numar par de cifre");
+            }
+        }
+        void genereaza(Graphics g)
+        {
+           // xfinalbare = 0;
+            if (nrparcifreok)
+            {
+                xfinalbare = 0;
+                nrparcifreok = false;
+                periefolositaneagra = true;
+                for (i = 0; i < lungimetextprimit + 2; i++)
+                {
+                    for (j = 1; j < 6; j++)
+                    {
+                            if (cod[j, i] == 1)
+                            {
+                                xfinalbare = xfinalbare + latimegros;
+                            }
+                            if (cod[j, i] == 0)
+                            {
+                                xfinalbare = xfinalbare + latimesubtire;
+                            }
+                            periefolositaneagra = false;
+                    }
+                }//obtinere lungime cod de bare
+                if (aparechenar.Checked == true)
+                {
+                    g.FillRectangle(periealba, xbareretinut - 10, ybareretinut - 10, xfinalbare+20, ystartbare + inaltime) ;
+                    //g.FillRectangle(periealba,0, 0, pb.Width,pb.Height);
+
+                } // desenare fundal alb data este dorit
+
+                for (i = 0; i < lungimetextprimit + 2; i++)
+                {
+                    for (j = 1; j < 6; j++)
+                    {
+                        if (periefolositaneagra)
+                        {
+                            if (cod[j, i] == 1)
+                            {
+                                g.FillRectangle(perieneagra, xstartbare, ystartbare, latimegros, inaltime);
+                                xstartbare = xstartbare + latimegros;
+                            }
+                            if (cod[j, i] == 0)
+                            {
+                                g.FillRectangle(perieneagra, xstartbare, ystartbare, latimesubtire, inaltime);
+                                xstartbare = xstartbare + latimesubtire;
+                            }
+                            periefolositaneagra = false;
+                        }
+                        else
+                        {
+                            if (cod[j, i] == 1)
+                            {
+                                g.FillRectangle(periealba, xstartbare, ystartbare, latimegros, inaltime);
+                                xstartbare = xstartbare + latimegros;
+                            }
+                            if (cod[j, i] == 0)
+                            {
+                                g.FillRectangle(periealba, xstartbare, ystartbare, latimesubtire, inaltime);
+                                xstartbare = xstartbare + latimesubtire;
+                            }
+                            periefolositaneagra = true;
+                        }
+
+                    }
+                } // desenare cod de bare
+
+
+                xtextdescris = (pb.Width - (Convert.ToInt32(fontt.Size) * (lungimetextprimit + 2))+(lungimetextprimit+1)*4) / 2;
+                ytextdescris = ystartbare + inaltime - 3;
+
+
+                for (i = 0; i < lungimetextprimit + 2; i++)
+                {
+                    g.DrawString(cod[0,i].ToString(), fontt, perieneagra, xtextdescris, ytextdescris);
+                    xtextdescris = xtextdescris + Convert.ToInt32(fontt.Size)-4;
+                }//scris cod sub cod de bare
+               
+                pb.Refresh();
+            }
+        }
+        void imaginenoua()
+        {
+           
+            sumapare = 0;
+            sumaimpare = 0;
+            img = new Bitmap(pb.Width, pb.Height);
+            g = Graphics.FromImage(img);
+            g.Clear(Color.LightBlue);
+        }
+        private void butonopenfile_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "JPG|*.jpg" })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    img = new Bitmap(pb.Width, pb.Height);
+                    g = Graphics.FromImage(img);
+                    pb.Image = Image.FromFile(ofd.FileName);
+                    BarcodeReader reader = new BarcodeReader();
+                    var result = reader.Decode((Bitmap)pb.Image);
+                    if (result != null)
+                        txtBarcode.Text = result.ToString();
+                }
+            }
+        }
+    }
+}
